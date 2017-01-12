6563940dcfdb14ca10b99064564e8710cfc3ee36
AnastasiaGritsuk <natikana_@mail.ru>
2017-01-12 11:20:04 +0300

    theMessage as method in prototype

--- src/client/js/app-state.js
+++ src/client/js/app-state.js
@@ -3,25 +3,26 @@ function AppState() {
     this.mainUrl =  'http://localhost:8080/chat',
     this.history = [],
     this.token =  ''
-    this.theMessage = function(text){
-        return {
-            id: this.uniqueId(),
-            text:text,
-            user: this.user
-        }
-    }
 }
 
 AppState.prototype.isLocalUser = function (user){
     return user != this.user;
-}
+};
 
 AppState.prototype.uniqueId = function() {
     var date = Date.now();
     var random = Math.random() * Math.random();
 
     return Math.floor(date * random).toString();
-}
+};
+
+AppState.prototype.theMessage = function(text){
+    return {
+        id: this.uniqueId(),
+        text:text,
+        user: this.user
+    }
+};
 
 var appState = new AppState();
 
