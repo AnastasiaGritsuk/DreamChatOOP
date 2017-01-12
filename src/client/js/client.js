47a82147d51daab173fbb4ccb8cb021e2c709838
AnastasiaGritsuk <natikana_@mail.ru>
2017-01-12 22:32:47 +0300

    create function scope in client module

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -1,48 +1,48 @@
-function Client() {
-    
-}
+module.exports = (function () {
+    function Client() {}
 
-Client.prototype.ajax = function (method, url, data, continueWith, continueWithError) {
-    var xhr = new XMLHttpRequest();
+    Client.prototype.ajax = function (method, url, data, continueWith, continueWithError) {
+        var xhr = new XMLHttpRequest();
 
-    continueWithError = continueWithError || function (err) {
-            console.error(err);
+        continueWithError = continueWithError || function (err) {
+                console.error(err);
+            };
+        xhr.open(method || 'GET', url, true);
+
+        xhr.onload = ()=>{
+            if(xhr.readyState !==4)
+                return;
+
+            if(xhr.status !=200){
+                continueWithError('Error on the server side, response ' + xhr.status);
+                return;
+            }
+            if(this.isError(xhr.responseText)) {
+                continueWithError('Error on the server side, response ' + xhr.responseText);
+                return;
+            }
+            continueWith(xhr.responseText);
         };
-    xhr.open(method || 'GET', url, true);
+        xhr.ontimeout = function(){
+            continueWithError('Server timed out !');
+        };
+        xhr.onerror = function (e) {
+            continueWithError(e);
+        };
+        xhr.send(data);
+    };
+    Client.prototype.isError = function (text) {
+        if(text == "")
+            return false;
 
-    xhr.onload = ()=>{
-        if(xhr.readyState !==4)
-            return;
-
-        if(xhr.status !=200){
-            continueWithError('Error on the server side, response ' + xhr.status);
-            return;
+        try{
+            var obj = JSON.parse(text);
+        }catch(ex){
+            return true;
         }
-        if(this.isError(xhr.responseText)) {
-            continueWithError('Error on the server side, response ' + xhr.responseText);
-            return;
-        }
-        continueWith(xhr.responseText);
-    };
-    xhr.ontimeout = function(){
-        continueWithError('Server timed out !');
-    };
-    xhr.onerror = function (e) {
-        continueWithError(e);
-    };
-    xhr.send(data);
-};
-Client.prototype.isError = function (text) {
-    if(text == "")
-        return false;
 
-    try{
-        var obj = JSON.parse(text);
-    }catch(ex){
-        return true;
-    }
+        return !!obj.error;
+    };
 
-    return !!obj.error;
-};
-
-module.exports = Client;
+    return Client;
+})();
