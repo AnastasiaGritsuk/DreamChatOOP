4c2e8c67cce71c877c7f9e35e986da37f30c6adf
AnastasiaGritsuk <natikana_@mail.ru>
2017-01-12 23:20:12 +0300

    create function scope in documentView module

--- src/client/js/documentView.js
+++ src/client/js/documentView.js
@@ -1,119 +1,122 @@
 var Emitter = require('component-emitter');
 
-function DocumentView() {
-    Emitter(this);
-    this.historyBox = document.getElementById('chatBoxId');
-    this.inputUsername = document.getElementsByClassName('icon-input')[0];
-    this.username = document.getElementById('username');
-    this.sendButton = document.getElementById('sendButton');
-    this.newMessageBox = document.getElementById('msgBox_textareaId');
-    document.addEventListener('DOMContentLoaded', ()=>{
-        this.emit('ready');
-    });
-    document.addEventListener('click', (evtObj)=>{
-        this.delegateEvent(evtObj);
-    });
-}
-
-DocumentView.prototype.isProperElement = function(e, classname){
-    return e.path[1].className === classname;
-}
-
-DocumentView.prototype.delegateEvent = function (evtObj){
-    if(evtObj.type == 'click' && this.isProperElement(evtObj, 'btn sendButton')) {
-        this.emit('sendButtonClick', evtObj);
-        return;
-    }
-    if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon edit')) {
-        this.emit('editClick', evtObj);
-        return;
-    }
-    if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon cancel')) {
-        this.emit('editCancelClick', evtObj);
-        return;
-    }
-    if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon complete')) {
-        this.emit('editCompleteClick', evtObj);
-        return;
-    }
-    if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon delete')) {
-        this.emit('deleteClick', evtObj);
-        return;
-    }
-    if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon editOn-username')) {
-        this.emit('editUsernameClick', evtObj);
-        return;
+module.exports = (function () {
+    function DocumentView() {
+        Emitter(this);
+        this.historyBox = document.getElementById('chatBoxId');
+        this.inputUsername = document.getElementsByClassName('icon-input')[0];
+        this.username = document.getElementById('username');
+        this.sendButton = document.getElementById('sendButton');
+        this.newMessageBox = document.getElementById('msgBox_textareaId');
+        document.addEventListener('DOMContentLoaded', ()=>{
+            this.emit('ready');
+        });
+        document.addEventListener('click', (evtObj)=>{
+            this.delegateEvent(evtObj);
+        });
     }
 
-    if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon editOff-username')) {
-        this.emit('editCompleteUsernameClick', evtObj);
-        return;
-    }
-};
+    DocumentView.prototype.isProperElement = function(e, classname){
+        return e.path[1].className === classname;
+    };
 
-DocumentView.prototype.render = function (modelRoot) {
-    if(modelRoot.history.length === 0)
-        return;
+    DocumentView.prototype.delegateEvent = function (evtObj){
+        if(evtObj.type == 'click' && this.isProperElement(evtObj, 'btn sendButton')) {
+            this.emit('sendButtonClick', evtObj);
+            return;
+        }
+        if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon edit')) {
+            this.emit('editClick', evtObj);
+            return;
+        }
+        if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon cancel')) {
+            this.emit('editCancelClick', evtObj);
+            return;
+        }
+        if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon complete')) {
+            this.emit('editCompleteClick', evtObj);
+            return;
+        }
+        if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon delete')) {
+            this.emit('deleteClick', evtObj);
+            return;
+        }
+        if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon editOn-username')) {
+            this.emit('editUsernameClick', evtObj);
+            return;
+        }
 
-    var msgMap = modelRoot.history.reduce(function(accumulator, msg){
-        accumulator[msg.id] = msg;
+        if(evtObj.type == 'click' && this.isProperElement(evtObj, 'icon editOff-username')) {
+            this.emit('editCompleteUsernameClick', evtObj);
+            return;
+        }
+    };
 
-        return accumulator;
-    },{});
+    DocumentView.prototype.render = function (modelRoot) {
+        if(modelRoot.history.length === 0)
+            return;
 
-    this.updateList(this.historyBox, msgMap);
-    this.appendToList(this.historyBox, modelRoot.history, msgMap, modelRoot);
-};
+        var msgMap = modelRoot.history.reduce(function(accumulator, msg){
+            accumulator[msg.id] = msg;
 
-DocumentView.prototype.updateList = function (element, msgMap) {
-    var children = element.children;
+            return accumulator;
+        },{});
 
-    for(var i=0;i<children.length;i++){
-        var child = children[i];
-        var id = child.attributes['id'].value;
-        var item = msgMap[id];
-        this.renderItemState(child.shadowRoot.children[1], item);
-        msgMap[id] = null;
-    }
-};
-DocumentView.prototype.appendToList = function (element, items, msgMap, modelRoot) {
-    for(var i=0; i<items.length; i++){
-        var item = items[i];
+        this.updateList(this.historyBox, msgMap);
+        this.appendToList(this.historyBox, modelRoot.history, msgMap, modelRoot);
+    };
 
-        if(msgMap[item.id] == null)
-            continue;
+    DocumentView.prototype.updateList = function (element, msgMap) {
+        var children = element.children;
 
-        msgMap[item.id] = null;
+        for(var i=0;i<children.length;i++){
+            var child = children[i];
+            var id = child.attributes['id'].value;
+            var item = msgMap[id];
+            this.renderItemState(child.shadowRoot.children[1], item);
+            msgMap[id] = null;
+        }
+    };
+    DocumentView.prototype.appendToList = function (element, items, msgMap, modelRoot) {
+        for(var i=0; i<items.length; i++){
+            var item = items[i];
 
-        var msgWpapper = document.createElement('div');
-        msgWpapper.setAttribute('id', item.id);
-        this.historyBox.appendChild(msgWpapper);
+            if(msgMap[item.id] == null)
+                continue;
 
-        var root1 = document.getElementById(item.id).createShadowRoot();
-        var template = this.msgFromTemplate(modelRoot.isLocalUser(item.user));
-        this.renderItemState(template.children[1], item);
+            msgMap[item.id] = null;
 
-        root1.appendChild(template);
-    }
-};
+            var msgWpapper = document.createElement('div');
+            msgWpapper.setAttribute('id', item.id);
+            this.historyBox.appendChild(msgWpapper);
 
-DocumentView.prototype.msgFromTemplate = function (mode) {
-    var template = document.getElementById('msg-template');
-    var clone = document.importNode(template.content, true);
+            var root1 = document.getElementById(item.id).createShadowRoot();
+            var template = this.msgFromTemplate(modelRoot.isLocalUser(item.user));
+            this.renderItemState(template.children[1], item);
 
-    if(mode){
-        clone.children[1].classList.add('other');
-    }
+            root1.appendChild(template);
+        }
+    };
 
-    return clone;
-};
+    DocumentView.prototype.msgFromTemplate = function (mode) {
+        var template = document.getElementById('msg-template');
+        var clone = document.importNode(template.content, true);
 
-DocumentView.prototype.renderItemState = function (item, message) {
-    item.setAttribute('id', message.id);
-    item.dataset.state = message.status;
-    item.getElementsByClassName('msg-username')[0].innerHTML = message.user;
-    item.getElementsByClassName('msg-text')[0].innerHTML = message.text;
-    item.getElementsByClassName('msg-time')[0].innerHTML = message.time;
-};
+        if(mode){
+            clone.children[1].classList.add('other');
+        }
 
-module.exports = DocumentView;
+        return clone;
+    };
+
+    DocumentView.prototype.renderItemState = function (item, message) {
+        item.setAttribute('id', message.id);
+        item.dataset.state = message.status;
+        item.getElementsByClassName('msg-username')[0].innerHTML = message.user;
+        item.getElementsByClassName('msg-text')[0].innerHTML = message.text;
+        item.getElementsByClassName('msg-time')[0].innerHTML = message.time;
+    };
+    
+    return DocumentView;
+})();
+
