e0b5c799d3f694ea67fb055bf9e8554c8bd3efdc
AnastasiaGritsuk <natikana_@mail.ru>
2017-01-12 23:22:49 +0300

    create function scope in appstate module

--- src/client/js/app-state.js
+++ src/client/js/app-state.js
@@ -1,27 +1,29 @@
-function AppState() {
-    this.user = 'User' + this.uniqueId(),
-    this.mainUrl =  'http://localhost:8080/chat',
-    this.history = [],
-    this.token =  ''
-}
-
-AppState.prototype.isLocalUser = function (user){
-    return user != this.user;
-};
-
-AppState.prototype.uniqueId = function() {
-    var date = Date.now();
-    var random = Math.random() * Math.random();
-
-    return Math.floor(date * random).toString();
-};
-
-AppState.prototype.theMessage = function(text){
-    return {
-        id: this.uniqueId(),
-        text:text,
-        user: this.user
+module.exports = (function () {
+    function AppState() {
+        this.user = 'User' + this.uniqueId(),
+        this.mainUrl =  'http://localhost:8080/chat',
+        this.history = [],
+        this.token =  ''
     }
-};
 
-module.exports = AppState;
+    AppState.prototype.isLocalUser = function (user){
+        return user != this.user;
+    };
+
+    AppState.prototype.uniqueId = function() {
+        var date = Date.now();
+        var random = Math.random() * Math.random();
+
+        return Math.floor(date * random).toString();
+    };
+
+    AppState.prototype.theMessage = function(text){
+        return {
+            id: this.uniqueId(),
+            text:text,
+            user: this.user
+        }
+    };
+    
+    return AppState;
+})();
